366d7b70750ae102a7a7f73a0eb1cdc7cb80f2cf
Solveig Thrun <solveig.thrun@web.de>
2026-02-17 09:22:30 +0100

    Update train_risk_prediction.py

--- src/train/train_risk_prediction.py
+++ src/train/train_risk_prediction.py
@@ -3,7 +3,7 @@ import torch
 import wandb
 from accelerate import Accelerator
 
-from models import MammoRegNet, LongitudinalMultiViewRiskModel
+from models import MammoRegNet
 from utils import create_logger, bootstrap_auc, bootstrap_c_index
 
 from models.model_factory import get_model
