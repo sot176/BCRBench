519a7c886842bc52c0e599abfd7404c6d3b5ac98
Solveig Thrun <solveig.thrun@web.de>
2026-02-27 13:33:19 +0100

    Update model.py

--- src/models/Mirai/model.py
+++ src/models/Mirai/model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from model_utils import ResNet18Backbone, SimpleTransformer, load_model
+from .model_utils import ResNet18Backbone, SimpleTransformer, load_model
 
 
 class MiraiFull(nn.Module):
