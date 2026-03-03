6a2e352e3076197eabfd5b7e6a3ffa9dd8669e31
Solveig Thrun <solveig.thrun@web.de>
2026-03-03 16:30:21 +0100

    Update resnet_base.py

--- src/models/Mirai/onconet/models/resnet_base.py
+++ src/models/Mirai/onconet/models/resnet_base.py
@@ -218,6 +218,7 @@ class ResNet(nn.Module):
 
     def aggregate_and_classify(self, x, risk_factors=None):
         # Pooling layer
+        print("arg risk factors", self.args.use_risk_factors)
         if self.args.use_risk_factors:
             logit, hidden = self.pool(x, risk_factors)
         else:
