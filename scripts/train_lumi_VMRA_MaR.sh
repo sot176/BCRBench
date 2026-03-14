ccfd64d646122a978d5eef05f1f0d4277a0486c7
Solveig Thrun <solveig.thrun@web.de>
2026-03-14 13:35:20 +0100

    Update train_lumi_VMRA_MaR.sh

--- scripts/train_lumi_VMRA_MaR.sh
+++ scripts/train_lumi_VMRA_MaR.sh
@@ -40,7 +40,7 @@ set -xv  # Print commands for debugging
 
 # Run the training script using Singularity
 export PYTHONPATH=$WORKING_DIR
-accelerate launch  main_train.py \
+accelerate launch --mixed_precision=fp16 main_train.py \
             --csv_file /scratch/project_465002309/thrunsol/embed_datasets/combined_cases_with_followup.csv \
             --data_root /scratch/project_465002309/thrunsol/embed_datasets/risk_dataset_1664_2048 \
             --path_out_dir /scratch/project_465002309/thrunsol/LMV_Risk_prediction_training_results_1664_2048_test_unified_github/embed/$SLURM_JOB_NAME-$SLURM_JOB_ID \
