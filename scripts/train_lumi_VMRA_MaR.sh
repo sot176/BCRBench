fb2c4819b4cb81aa7c2767749a4c444d46f085de
Solveig Thrun <solveig.thrun@web.de>
2026-03-16 18:23:15 +0100

    Update train_lumi_VMRA_MaR.sh

--- scripts/train_lumi_VMRA_MaR.sh
+++ scripts/train_lumi_VMRA_MaR.sh
@@ -50,7 +50,7 @@ accelerate launch --mixed_precision=fp16  main_train.py \
             --batch_size 2 \
             --augmentations "True" \
             --num_workers 7 \
-            --learning_rate 1e-4 \
+            --learning_rate 5e-5 \
             --weight_decay 1e-4 \
             --model "VMRA-MaR" \
             --use_asymmetry \
