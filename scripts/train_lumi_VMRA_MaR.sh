43ff65b8131ee2cddc25a4ffa652c7169ba09d30
Solveig Thrun <solveig.thrun@web.de>
2026-03-18 10:43:26 +0100

    Update train_lumi_VMRA_MaR.sh

--- scripts/train_lumi_VMRA_MaR.sh
+++ scripts/train_lumi_VMRA_MaR.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=56
 #SBATCH --mem=480G
-#SBATCH --time=01:59:00               # Run time (hh:mm:ss)
+#SBATCH --time=12:59:00               # Run time (hh:mm:ss)
 #SBATCH --account=project_465002309     # Project for billing
 
 
