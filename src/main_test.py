130bb90cfe2258f0b3d3d90ef6bd61688be0968c
Solveig Thrun <solveig.thrun@web.de>
2026-04-08 20:32:34 +0200

    new

--- src/main_test.py
+++ src/main_test.py
@@ -36,7 +36,7 @@ def parse_test_args():
 
     # ---------------- Model-specific YAML ----------------
     try:
-        yaml_path = f"config/model/{temp_args.model.lower().replace('-', '_')}.yaml"
+        yaml_path = f"config/models/{temp_args.model.lower().replace('-', '_')}.yaml"
         with open(yaml_path, "r") as f:
             model_config = yaml.safe_load(f)
 
