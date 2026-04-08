5fbd1c05e2736a7b72818e73f582fa3e935a1ce5
Solveig Thrun <solveig.thrun@web.de>
2026-04-08 20:25:16 +0200

    Update main_train.py

--- src/main_train.py
+++ src/main_train.py
@@ -81,6 +81,7 @@ def parse_cli_args():
     # ---------------- Model-specific YAML ----------------
     try:
         yaml_path = f"configs/model/{temp_args.model.lower().replace('-', '_')}.yaml"
+        print("yaml path", yaml_path)
         with open(yaml_path, "r") as f:
             model_config = yaml.safe_load(f)
 
