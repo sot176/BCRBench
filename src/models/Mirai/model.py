36c7b2dfb1dd0651dc318ba94a786e92fda758fc
Solveig Thrun <solveig.thrun@web.de>
2026-04-28 13:55:39 +0200

    Update model.py

--- src/models/Mirai/model.py
+++ src/models/Mirai/model.py
@@ -147,7 +147,7 @@ class Mirai(BaseRiskModel):
             None,
             transformer_batch
         )
-        print("Transformer output shape:", pooled_hidden.shape)
+        pooled_hidden = pooled_hidden.mean(dim=1) 
         logit = self.risk_head(pooled_hidden)
 
         return logit, pooled_hidden, activ
